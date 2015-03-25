bb76b9c9d5a9d17643fa12fa833eb243a16ae0d1
Aoibheann Farrell <aoibheann.farrell@gmail.com>
2015-03-25 12:31:06 +0000

    Added Company Controller

---
+++ timeTracker.Domain/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace timeTracker.Domain
+{
+   public  class Company
+    {
+       public virtual int Id { get; set; }
+       [Required]
+       public virtual string Name { get; set; }
+       [Required]
+       [Display(Name = "Contact Person")]
+       public virtual string Contactperson { get; set; }
+       [Required]
+       [Display(Name = "Contact Number")]
+       public virtual string Contactnumber { get; set; }
+       [Display(Name = "Contact Email")]
+       public virtual string Contactemail { get; set; }
+       public virtual string Description { get; set; }
+       public virtual ICollection<Project> Projects { get; set; } 
+    }
+}
